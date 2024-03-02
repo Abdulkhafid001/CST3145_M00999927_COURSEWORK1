070001ead9f2289b541861982491f81d9c248f95
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2024-03-02 10:04:43 +0400

    fixed lessons.js file

---
+++ lessons.js
@@ -0,0 +1,11 @@
+// all courses must be stored as an array of JSON objects to be passed to the VUE app
+course1 = "{'subject': 'learn HTML', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course2 = "{'subject': 'Build Mobile Apps', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course3 = "{'subject': 'strategic information systems', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course4 = "{'subject': 'business intelligence', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course5 = "{'subject': 'ui/ux', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course6 = "{'subject': 'building modern websites', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course7 = "{'subject': 'introduction to java se', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course8 = "{'subject': 'data structures and algorithms in python', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course9 = "{'subject': 'video editing', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
+course10 = "{'subject': 'crypto trading', 'location':'','price':'','tutor':'','spaces':'link to FA icon'}";
